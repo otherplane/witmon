88506ac8a52d244d3cd19f20b210726843ef378b
Guillermo Díaz <guillermo@otherplane.com>
2021-10-07 18:32:54 +0200

    fix(core): assert range in `randomUniform` and rehashing on iteration

--- packages/core/contracts/WitmonDecoratorBase.sol
+++ packages/core/contracts/WitmonDecoratorBase.sol
@@ -22,15 +22,18 @@ abstract contract WitmonDecoratorBase
         baseURI = _baseURI;
     }
 
-    /// @dev ...
+    /// @dev Generates pseudo-random number uniformly distributed in range [0 .. _range).
     function randomUniform(bytes32 _phenotype, uint256 _seed, uint8 _range)
         public pure
         virtual
         returns (uint8 _number)
     {
+        assert(_range > 0);
+        uint8 _mask = uint8(uint256(2 ** _logBaseBits(_range) - 1));
         bytes32 _hash = keccak256(abi.encode(_phenotype, _seed));
         do {
-            _number = uint8(uint256(keccak256(abi.encode(_hash))));
+            _hash = keccak256(abi.encode(_hash));
+            _number = uint8(uint256(_hash)) & _mask;
         } while (_number >= _range);
     }
 
@@ -44,4 +47,13 @@ abstract contract WitmonDecoratorBase
         uint8 _mask = uint8(uint256(2 ** _bits) - 1);
         return uint8(uint256(keccak256(abi.encode(_phenotype, _seed)))) & _mask;
     }	
+
+    /// @dev Returns size in bits of given value
+    function _logBaseBits(uint8 _range) internal pure returns (uint _bits) {
+        uint _target = 1;
+        while (_target <= _range) {
+            _bits ++;
+            _target <<= 1;
+        }
+    }
 }
