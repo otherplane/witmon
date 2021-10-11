4765c0e3c4b53711f0eec1aa25bcee15c013be6f
Mario Cao <mariocao@gmail.com>
2021-10-11 17:52:30 +0200

    fix(ui): change route mode

--- packages/ui/src/router/index.js
+++ packages/ui/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import MyEgg from '../views/MyEgg.vue'
 import InitGame from '../views/InitGame.vue'
@@ -106,7 +106,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
