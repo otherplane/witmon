12e927e5cbb0ac0a0d4f2a147aefab71c3c951eb
Mario Cao <mariocao@gmail.com>
2021-10-15 11:56:37 +0200

    test(api): skip metadata test

--- packages/api/test/routes/metadata.test.ts
+++ packages/api/test/routes/metadata.test.ts
@@ -1,25 +1,25 @@
-import { test } from 'tap'
+import { skip } from 'tap'
 
 import { server } from './helper'
 
 // Note: this test requires WEB3_PROVIDER and a deployed WitmonERCC721 contract
-// test('should get metadata for egg #77', async (t) => {
-//   await new Promise((resolve) => {
-//     server.inject(
-//       {
-//         method: 'GET',
-//         url: `/metadata/1`,
-//       },
-//       (err, response) => {
-//         t.error(err)
-//         t.equal(response.statusCode, 200)
-//         t.equal(
-//           response.headers['content-type'],
-//           'application/json; charset=utf-8'
-//         )
-
-//         resolve(true)
-//       }
-//     )
-//   })
-// })
+skip('should get metadata for egg #1', async (t) => {
+  await new Promise((resolve) => {
+    server.inject(
+      {
+        method: 'GET',
+        url: `/metadata/1`,
+      },
+      (err, response) => {
+        t.error(err)
+        t.equal(response.statusCode, 200)
+        t.equal(
+          response.headers['content-type'],
+          'application/json; charset=utf-8'
+        )
+        t.end()
+        resolve(true)
+      }
+    )
+  })
+})
