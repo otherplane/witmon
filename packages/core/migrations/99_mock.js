92e6d597b41395c5a688a34c90a0ecaa48e2c665
Guillermo Díaz <guillermo@otherplane.com>
2021-10-17 14:47:18 +0200

    chore(core): move WitmonMock as test helper

--- packages/core/migrations/99_mock.js
+++
@@ -1,9 +0,0 @@
-const WitmonMock = artifacts.require("WitmonMock")
-
-module.exports = async function (deployer, network) {
-
-  await deployer.deploy(
-    WitmonMock,
-    "0x12890D2cce102216644c59daE5baed380d84830c" // signator
-  )
-};
