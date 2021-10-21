cb7a4419786321deba94aa37e80e74539c3e0428
Mario Cao <mariocao@gmail.com>
2021-10-21 20:01:05 +0200

    chore(ui): update constants

--- packages/ui/src/constants.js
+++ packages/ui/src/constants.js
@@ -1,18 +1,18 @@
 export const CONTRACT_ADDRESS =
   import.meta.env.CONTRACT_ADDRESS ||
-  '0x691908f883E006C0fB42da190A9EA07E6996D6c6'
+  '0x855BCa56D00F3f550D0c610BBF562FEBF6540bc6'
 
 export const OPENSEA_BASE_URL =
   import.meta.env.OPENSEA_BASE_URL ||
-  'https://testnets.opensea.io/assets/0x691908f883e006c0fb42da190a9ea07e6996d6c6'
+  'https://opensea.io/assets/0x855BCa56D00F3f550D0c610BBF562FEBF6540bc6'
 
 export const ETHERSCAN_BASE_URL =
-  import.meta.env.ETHERSCAN_BASE_URL || 'https://rinkeby.etherscan.io/tx'
+  import.meta.env.ETHERSCAN_BASE_URL || 'https://etherscan.io/tx'
 
-export const NETWORK = import.meta.env.NETWORK || 'rinkeby'
+export const NETWORK = import.meta.env.NETWORK || 'main'
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:3000'
+  import.meta.env.VITE_API_BASE_URL || 'https://api-liscon21.wittycreatures.com'
 
 export const BASE_URL =
   import.meta.env.VITE_BASE_URL || 'https://wittycreatures.com'
